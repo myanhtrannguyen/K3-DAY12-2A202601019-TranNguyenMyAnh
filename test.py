cdae9094da02ca92a860d2f66c60a32cbae48b7b
myanhtrannguyen <myanh260305@gmail.com>
2026-08-10 23:42:29 +0700

    fix

--- test.py
+++
@@ -1,6 +0,0 @@
-for i in $(seq 1 5); do
-    curl -s -X POST http://localhost:8000/ask \
-        -H "Content-Type: application/json" \
-        -H. "X-API-Key: $AGENT_API_KEY" \
-        -d. '{"question": "Xin chào lần '"$i"'"}' | python3 -m json.tool
-    echo. "---"
